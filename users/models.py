a91562c0ed14eeffb2f06d9eeafb7a9b6b910916
Abduqod1rr <ttheonepersonn@gmail.com>
2025-10-06 21:09:09 +0500

    .

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class CustomUser(AbstractUser,models.Model):
+    ROLE_CHOICES=[
+        ("Teacher","teacher"),
+        ("Student","student"),
+    ]
+    
+    role=models.CharField(choices=ROLE_CHOICES, max_length=50,default="Student")
