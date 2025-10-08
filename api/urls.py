bbe8a959ab27e2a3ce912d3cb09b84c557264bfa
Abduqod1rr <ttheonepersonn@gmail.com>
2025-10-08 21:01:35 +0500

    .

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from drf_spectacular.views import SpectacularAPIView ,SpectacularSwaggerView
 
 urlpatterns = [
     path("c_lesson/", views.createLesson.as_view(), name="c_lesson"),
     path("l-crud/<int:pk>", views.lesson_crud.as_view(), name="l-crud"),
     path("buy-lesson/<int:pk>", views.Buylessons.as_view(), name="buy"),
     path("home/", views.seeLessons.as_view(), name="see-lessons"),
-    path("see-b-lessons/", views.seeBoughtLessons.as_view(), name="")
+    path("see-b-lessons/", views.seeBoughtLessons.as_view(), name=""),
+    path("docs/ui/", SpectacularAPIView.as_view(), name="swagger")
 ]
