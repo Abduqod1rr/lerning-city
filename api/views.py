6a7706e5cea9e0d4b0fdee2f9ced08d5c38e58a9
Abduqod1rr <ttheonepersonn@gmail.com>
2025-10-11 19:31:37 +0500

    with docker

--- api/views.py
+++ api/views.py
@@ -4,29 +4,31 @@ from .serializers import lesson_sz ,boughtlessons_sz
 from .permissions import IsOwner , IsStudent
 from rest_framework import generics
 
-#
+#creating lesson
 class createLesson(generics.CreateAPIView):
 
     queryset=Lesson.objects.all()
     serializer_class=lesson_sz
 
-#
+#CRUD lessons
 class lesson_crud(generics.RetrieveUpdateDestroyAPIView):
     
     queryset=Lesson.objects.all()
     serializer_class=lesson_sz
     permission_classes=[IsOwner]
 
-#
+#buy lesson
 class Buylessons(generics.CreateAPIView):
     queryset = boughtLessons.objects.all()
     serializer_class = boughtlessons_sz
 
+#home
 class seeLessons(generics.ListAPIView):
 
     queryset=Lesson.objects.all()
     serializer_class=lesson_sz
 
+#my bought lessons
 class seeBoughtLessons(generics.ListAPIView):
     queryset = boughtLessons.objects.all()
     serializer_class = boughtlessons_sz
